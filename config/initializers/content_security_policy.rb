1b5dcb6e900992b318195bf90f8252ca469abfac
Obed-Edom Nkezabahizi <onkezabahizi@gmail.com>
2022-04-08 18:04:51 +0200

    CSP report only set true in config.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,7 +7,7 @@
 Rails.application.config.content_security_policy do |policy|
    policy.default_src :self, :https
    policy.font_src    :self, :https, :data
-   policy.img_src     :self, :https, :http, :data, "*"
+   policy.img_src     :self, :https, "https://laastras.herokuapp.com", "http://localhost:3000"
    policy.object_src  :self, :https, :data
    policy.script_src  :self, :https
    policy.style_src   :self, :https, :data, "*"
@@ -27,4 +27,4 @@ Rails.application.config.content_security_policy do |policy|
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy_report_only = true
