47b3ffbe34414990de638664586c081cc1e23cb9
Obed-Edom Nkezabahizi <onkezabahizi@gmail.com>
2022-04-10 14:35:11 +0200

    background image in both logistics and e_commerce controllers.

--- app/controllers/e_commerce_controller.rb
+++ app/controllers/e_commerce_controller.rb
@@ -24,5 +24,9 @@ class ECommerceController < ApplicationController
         @founder_and_ceo_contact_label = I18n.t 'founder_and_ceo_contact_label'
         @work_in_progress_description = I18n.t 'work_in_progress_description'
         ApplicationHelper.set_user_set_locale(session)
+        #http://getwallpapers.com/wallpaper/full/f/9/0/838457-full-size-outdoors-wallpapers-1920x1200.jpg
+        @site_background_image_url = ApplicationHelper.image_asset_url(
+            request, '838457-default-background-image.jpg'
+        )
     end
 end
