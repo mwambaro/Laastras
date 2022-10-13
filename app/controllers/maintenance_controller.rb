20fe3cf4d08f16b1dae76451b95547fc1647042c
Obed-Edom Nkezabahizi <onkezabahizi@gmail.com>
2022-10-13 08:53:42 +0200

    Previous commit did not fix our issue. SOLUTION: delete the 'db/schema.rb' file, run a hard reset using 'rails db:reset' followed by 'rails db:migrate'.

--- app/controllers/maintenance_controller.rb
+++ app/controllers/maintenance_controller.rb
@@ -2,20 +2,51 @@ class MaintenanceController < ApplicationController
     before_action :init_parameters
 
     def fail_safe
-    end
+        begin 
+            unless session[:fail_safe_message].nil? 
+                unless session[:fail_safe_message].blank?
+                    @fail_safe_message = session[:fail_safe_message]
+                end 
+            end
+            if @fail_safe_message.nil? || @fail_safe_message.blank? 
+                @fail_safe_message = (I18n.t 'fail_safe_message')
+            end
+
+            unless session[:fail_safe_title].nil? 
+                unless session[:fail_safe_title].blank?
+                    @fail_safe_title = session[:fail_safe_title]
+                end 
+            end
+            if @fail_safe_title.nil? || @fail_safe_title.blank?
+                @fail_safe_title = (I18n.t 'fail_safe_title')
+            end
+        rescue Exception => e 
+            message = Time.now.to_s + ": " + Pathname.new(__FILE__).basename.to_s + "#" + 
+                    __method__.to_s + "--- " + e.message 
+            logger.debug message unless logger.nil?
+        end
+
+    end # fail_safe
 
     def init_parameters 
-        I18n.locale = session[:active_language].to_sym unless session[:active_language].nil?
-        @site_title = "Laastras | #{params[:action]}"
-        @laastras_banner_image = ApplicationHelper.image_asset_url(
-            request, 'Laastras-e-banner-lg.JPG'
-        )
-        @open_graph_proto_image_url = ApplicationHelper.image_asset_url(
-            request, 'Laastras-e-banner-lg.JPG'
-        )
-        @logo_image_url = ApplicationHelper.image_asset_url(
-            request, 'Logo-03.svg'
-        )
-    end
+        begin
+            I18n.locale = session[:active_language].to_sym unless session[:active_language].nil?
+            @site_title = "Laastras | #{params[:action]}"
+            @laastras_banner_image = ApplicationHelper.image_asset_url(
+                request, 'Laastras-e-banner-lg.JPG'
+            )
+            @open_graph_proto_image_url = ApplicationHelper.image_asset_url(
+                request, 'Laastras-e-banner-lg.JPG'
+            )
+            @logo_image_url = ApplicationHelper.image_asset_url(
+                request, 'Logo-03.svg'
+            )
+        rescue Exception => e 
+            message = Time.now.to_s + ": " + Pathname.new(__FILE__).basename.to_s + "#" + 
+                    __method__.to_s + "--- " + e.message 
+            logger.debug message unless logger.nil?
+        end
+
+    end # init_parameters
     
 end
